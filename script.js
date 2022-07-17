9239d6c5a0eff6ae59404631e0ad60bed2ecf25a
Иззатилло Турсунов <izzatillotursunov@MacBook-Pro-Izzatillo.local>
2022-07-17 14:38:19 +0500

    My change

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ let personalMovieDB = {
   private:false  
 };
 const a = prompt("Один из последних просмотренный фильмов?", ""),
-      c = prompt("На сколько оцените его?",""),
+      c = prompt("На сколько ояяяцените его?",""),
       b = prompt("Один из последних просмотренный фильмов?", ""),
-      d = prompt("На сколько оцените его?","");
+      d = prompt("На сколько оцените его?aa","");
 personalMovieDB.movies[a] = c ;
 personalMovieDB.movies[b] = d ;
 console.log(personalMovieDB);
